11ee66eed4d2bd1d6b821220417212166df7b7a2
janaYahyaa <jana7yehia@gmail.com>
2026-03-17 13:01:31 +0200

    feat(users) add service function that gets current user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,4 +4,10 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+
+  getCurrentUser(userId: string) {
+    return this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+  }
 }
