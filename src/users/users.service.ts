f595c34b93edc6f1babcbc768b162411f5f147e5
janaYahyaa <jana7yehia@gmail.com>
2026-03-18 04:32:34 +0200

    feat(users): enhance getCurrentUser method to include error handling and return detailed user data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,15 +2,66 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { PrivateUserDto } from './dto/private-user.dto';
 import { PublicUserDto } from './dto/public-user.dto';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  getCurrentUser(userId: string) {
-    return this.prisma.user.findUnique({
+  async getCurrentUser(userId: string): Promise<UserDto> {
+    const user = await this.prisma.user.findUnique({
       where: { id: userId },
+      select: {
+        id: true,
+        username: true,
+        display_name: true,
+        email: true,
+        role: true,
+        bio: true,
+        location: true,
+        avatar_url: true,
+        cover_url: true,
+        created_at: true,
+        visibility: true,
+        is_active: true,
+        is_verified: true,
+        updated_at: true,
+        last_login_at: true,
+      },
     });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const [tracksCount, followersCount, followingCount, likesReceived] =
+      await Promise.all([
+        this.prisma.track.count({ where: { userId: userId } }),
+        this.prisma.follow.count({ where: { followingId: userId } }),
+        this.prisma.follow.count({ where: { followerId: userId } }),
+        this.prisma.trackLike.count({
+          where: { track: { userId: userId } },
+        }),
+      ]);
+    return {
+      id: user.id,
+      username: user.username,
+      displayName: user.display_name,
+      email: user.email,
+      role: user.role,
+      bio: user.bio,
+      location: user.location,
+      avatarUrl: user.avatar_url,
+      coverUrl: user.cover_url,
+      createdAt: user.created_at,
+      updatedAt: user.updated_at,
+      lastLogin: user.last_login_at,
+      visibility: user.visibility,
+      isActive: user.is_active,
+      isVerified: user.is_verified,
+      tracksCount,
+      followersCount,
+      followingCount,
+      likesReceived,
+    };
   }
 
   async getUser(
