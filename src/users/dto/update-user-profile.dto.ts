589a6f66e07156110e98c5bf17a125d15931e6e4
janaYahyaa <jana7yehia@gmail.com>
2026-03-20 20:05:44 +0200

    feat(users): add DTOs for updating user profile and social links

---
+++ src/users/dto/update-user-profile.dto.ts
@@ -0,0 +1,54 @@
+import { UserType, Visibility } from '@prisma/client';
+import {
+  Matches,
+  IsEnum,
+  IsString,
+  IsUrl,
+  MinLength,
+  MaxLength,
+  IsOptional,
+} from 'class-validator';
+import { AtLeastOneField } from '../at-least-one-field.validator';
+
+@AtLeastOneField()
+export class UpdateUserProfileDto {
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  @MaxLength(30)
+  @Matches(/^[a-zA-Z0-9_]+$/, {
+    message: 'Username can only contain letters, numbers, and underscores',
+  })
+  username?: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(300)
+  bio?: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  @MaxLength(30)
+  display_name?: string;
+
+  @IsEnum(UserType)
+  @IsOptional()
+  role?: UserType;
+
+  @IsString()
+  @IsOptional()
+  location?: string;
+
+  @IsEnum(Visibility)
+  @IsOptional()
+  visibility?: Visibility;
+
+  @IsUrl()
+  @IsOptional()
+  avatar_url?: string;
+
+  @IsUrl()
+  @IsOptional()
+  cover_url?: string;
+}
